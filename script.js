733f71cb52c5950dfbbf8253a72d13ca89e038ee
Sourav Sarker <souravsarker036@gmail.com>
2025-09-08 01:17:28 +0600

    plant a tree

---
+++ script.js
@@ -0,0 +1,6 @@
+ // navbar mobile responsive link view
+  const menuBtn = document.getElementById('menu-btn');
+    const mobileMenu = document.getElementById('mobile-menu');
+    menuBtn.addEventListener('click', () => {
+        mobileMenu.classList.toggle('hidden');
+    });
